525bca434a163c3546b9d0675eb696557ac49d00
Rostyslav <rossgaluzinskiy@gmail.com>
2023-01-28 20:14:23 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -38,38 +38,4 @@ const Searchbar = ({ onSubmit }) => {
   );
 };
 
-// class Searchbar extends Component {
-//   state = {
-//     name: '',
-//   };
-
-//   onChangeInput = e => {
-//     this.setState({ name: e.currentTarget.value });
-//   };
-
-//   onSubmitForm = e => {
-//     e.preventDefault();
-//     this.props.onSubmit(this.state.name);
-//   };
-
-//   render() {
-//     return (
-//       <SearchbarHeader>
-//         <SearchForm onSubmit={this.onSubmitForm}>
-//           <SearchFormBtn type="submit">
-//             <FaSearch color="black" />
-//           </SearchFormBtn>
-//           <SearchFormInput
-//             type="text"
-//             value={this.state.name}
-//             onChange={this.onChangeInput}
-//             autocomplete="off"
-//             placeholder="Search images and photos"
-//           />
-//         </SearchForm>
-//       </SearchbarHeader>
-//     );
-//   }
-// }
-
 export default Searchbar;
