646cf7a4ba25f9a9af8951321b5e7b477e5284fd
Rostyslav <rossgaluzinskiy@gmail.com>
2023-01-29 11:43:55 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, Modal } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
 const LargePhotoModal = ({ largeImg, onClose }) => {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const onCloseModal = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
+  const onCloseModal = useCallback(
+    e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
   useEffect(() => {
     window.addEventListener('keydown', onCloseModal);
@@ -35,38 +37,5 @@ const LargePhotoModal = ({ largeImg, onClose }) => {
     modalRoot
   );
 };
-
-// class LargePhotoModal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.onCloseModal);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.onCloseModal);
-//   }
-
-//   onCloseModal = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleBackdropClick = e => {
-//     if (e.target === e.currentTarget) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     return createPortal(
-//       <Overlay onClick={this.handleBackdropClick}>
-//         <Modal>
-//           <img src={this.props.largeImg} alt="" />
-//         </Modal>
-//       </Overlay>,
-//       modalRoot
-//     );
-//   }
-// }
 
 export default LargePhotoModal;
